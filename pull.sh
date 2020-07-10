e0a9b61da5cd8f248311341c76e04a8bae0e38d1
ChillerDragon <ChillerDragon@gmail.com>
2020-07-10 15:14:19 +0200

    remove defaults

--- pull.sh
+++ pull.sh
@@ -4,9 +4,6 @@
 cd etc/apache2/sites-available || exit 1
 cp /etc/apache2/sites-available/paste.zillyhuhn.com-le-ssl.conf paste.zillyhuhn.com-le-ssl.conf
 cp /etc/apache2/sites-available/zillyhuhn.com.conf              zillyhuhn.com.conf
-cp /etc/apache2/sites-available/default-ssl.conf                default-ssl.conf
-cp /etc/apache2/sites-available/000-default.conf                000-default.conf
-cp /etc/apache2/sites-available/default-tls.conf                default-tls.conf
 cp /etc/apache2/sites-available/paste.zillyhuhn.com.conf        paste.zillyhuhn.com.conf
 cp /etc/apache2/sites-available/zillyhuhn.com-le-ssl.conf       zillyhuhn.com-le-ssl.conf
 
--- push.sh
+++ push.sh
@@ -12,9 +12,6 @@ function backup_cfg() {
 
 backup_cfg /etc/apache2/sites-available/paste.zillyhuhn.com-le-ssl.conf
 backup_cfg /etc/apache2/sites-available/zillyhuhn.com.conf
-backup_cfg /etc/apache2/sites-available/default-ssl.conf
-backup_cfg /etc/apache2/sites-available/000-default.conf
-backup_cfg /etc/apache2/sites-available/default-tls.conf
 backup_cfg /etc/apache2/sites-available/paste.zillyhuhn.com.conf
 backup_cfg /etc/apache2/sites-available/zillyhuhn.com-le-ssl.conf
 
@@ -35,9 +32,6 @@ fi
 cd etc/apache2/sites-available || exit 1
 cp paste.zillyhuhn.com-le-ssl.conf /etc/apache2/sites-available/paste.zillyhuhn.com-le-ssl.conf
 cp zillyhuhn.com.conf              /etc/apache2/sites-available/zillyhuhn.com.conf             
-cp default-ssl.conf                /etc/apache2/sites-available/default-ssl.conf               
-cp 000-default.conf                /etc/apache2/sites-available/000-default.conf               
-cp default-tls.conf                /etc/apache2/sites-available/default-tls.conf               
 cp paste.zillyhuhn.com.conf        /etc/apache2/sites-available/paste.zillyhuhn.com.conf       
 cp zillyhuhn.com-le-ssl.conf       /etc/apache2/sites-available/zillyhuhn.com-le-ssl.conf      
 
