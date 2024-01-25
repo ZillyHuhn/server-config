7737c85f1f11ede0f92de4eba3a2563d1c355172
Chiller Dragon <chillerdragon@gmail.com>
2024-01-25 14:47:40 +0800

    Merge certs

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# main site cert
-# /etc/letsencrypt/live/zillyhuhn.com/
-certbot certonly \
-     -d zillyhuhn.com -d www.zillyhuhn.com
-
 # subdomain wildcart cert
 # /etc/letsencrypt/live/zillyhuhn.com-0001/
 certbot certonly \
@@ -13,5 +8,4 @@ certbot certonly \
     --email chillerdragon@gmail.com \
     --server https://acme-v02.api.letsencrypt.org/directory \
     --agree-tos \
-    -d '*.zillyhuhn.com'
-
+    -d '*.zillyhuhn.com' -d zillyhuhn.com
